54e70831b96025817242b4471e0bbe04fa4de304
Unknown <p.thorn.ru@gmail.com>
2018-10-03 11:59:58 +0300

    Snackbars: fix import

--- src/messages-widgets/Snackbars.js
+++ src/messages-widgets/Snackbars.js
@@ -6,7 +6,7 @@ import IconButton from '@material-ui/core/IconButton';
 import { withStyles } from '@material-ui/core/styles';
 import CloseIcon from '@material-ui/icons/Close';
 
-import registry from '../../registry';
+import registry from '../registry';
 
 
 const styles = theme => ({
